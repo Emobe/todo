a39268b9d66322d7f6b70824cab31933160fdf50
Emobe <anthony.r.shuker@gmail.com>
2018-11-11 13:49:58 +0000

    modal + update react

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { List, Paper, Button } from '@material-ui/core';
 import { Add as AddIcon } from '@material-ui/icons';
-import { TodoItem } from 'components';
+import { TodoItem, TodoModal } from 'components';
 
 const styles = theme => ({
 	paper: {
@@ -20,27 +20,62 @@ const styles = theme => ({
 	}
 });
 
-const TodoList = ({ ...props }) => (
-	<Paper className={props.classes.paper}>
-		<List className={props.classes.list}>
-			{props.todos.length ? (
-				props.todos.map(todo => (
-					<TodoItem
-						key={todo.id}
-						toggle={() => props.toggleTodo(todo.id)}
-						name={todo.name}
-						done={todo.done ? todo.done : false}
-					/>
-				))
-			) : (
-				<span className="done">All caught up!</span>
-			)}
-		</List>
-		<Button variant="fab" color="secondary" className={props.classes.add}>
-			<AddIcon />
-		</Button>
-	</Paper>
-);
+class TodoList extends Component {
+	constructor(props) {
+		super(props);
+		this.openAddModal = this.openAddModal.bind(this);
+		this.closeAddModal = this.closeAddModal.bind(this);
+	}
+
+	static propTypes = {};
+
+	state = {
+		addModal: false
+	};
+
+	openAddModal() {
+		this.setState({ addModal: true });
+	}
+
+	closeAddModal() {
+		this.setState({ addModal: false });
+	}
+
+	render() {
+		const { classes, todos, toggleTodo, createTodo } = this.props;
+		return (
+			<Paper className={classes.paper}>
+				<List className={classes.list}>
+					{todos.length ? (
+						todos.map(todo => (
+							<TodoItem
+								key={todo.id}
+								toggle={() => toggleTodo(todo.id)}
+								name={todo.name}
+								done={todo.done ? todo.done : false}
+							/>
+						))
+					) : (
+						<span className="done">All caught up!</span>
+					)}
+				</List>
+				<Button
+					variant="fab"
+					color="secondary"
+					className={classes.add}
+					onClick={this.openAddModal}
+				>
+					<AddIcon />
+				</Button>
+				<TodoModal
+					open={this.state.addModal}
+					onClose={this.closeAddModal}
+					create={createTodo}
+				/>
+			</Paper>
+		);
+	}
+}
 
 TodoList.defaultProps = {
 	todos: [],
--- src/components/TodoModal/TodoModal.js
+++ src/components/TodoModal/TodoModal.js
@@ -1,29 +1,71 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Modal } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import {
+	Modal,
+	Typography,
+	TextField,
+	Grid,
+	Button,
+	Snackbar
+} from '@material-ui/core';
 
-class TodoModal extends Component {
-	propTypes = {};
-
-	state = {
-		open: 0,
-		todo: ''
-	};
-
-	constructor(props) {
-		super(props);
+const styles = theme => ({
+	paper: {
+		position: 'absolute',
+		background: theme.palette.background.paper,
+		boxShadow: theme.shadows[5],
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		padding: theme.spacing.unit * 4,
+		display: 'flex',
+		flexDirection: 'column',
+		height: '25vh',
+		justifyContent: 'space-around',
+		width: '20vw'
 	}
+});
 
-	render() {
-		return (
-			<Modal className="">
-				hello i am Modal
-				<br />
-				sdfgsdfs
-				<br />
-			</Modal>
-		);
-	}
-}
+const TodoModal = ({ ...props }) => {
+	const [name, setName] = useState('');
+	const [todoNotif, setTodoNotif] = useState(false);
+	return (
+		<Modal className="" open={props.open} onClose={props.onClose}>
+			<div className={props.classes.paper}>
+				<Typography variant="h6">Add Todo</Typography>
+				<TextField
+					value={name}
+					placeholder="Enter Todo..."
+					label="Todo"
+					onChange={e => setName(e.target.value)}
+				/>
+				<Button
+					variant="contained"
+					color="secondary"
+					onClick={() => {
+						props.create(name);
+						setTodoNotif(true);
+					}}
+				>
+					Add
+				</Button>
+				<Snackbar
+					anchor={{ vertical: 'bottom', horizontal: 'left' }}
+					open={todoNotif}
+					message={<span>Todo Added!</span>}
+					autoHideDuration={3000}
+					onClose={() => setTodoNotif(false)}
+				/>
+			</div>
+		</Modal>
+	);
+};
 
-export default TodoModal;
+TodoModal.propTypes = {
+	classes: PropTypes.object.isRequired,
+	open: PropTypes.bool.isRequired,
+	onClose: PropTypes.func.isRequired
+};
+
+export default withStyles(styles)(TodoModal);
