68bbd6fb1d3f80fe6bfacf52bd65f6d233b12d87
Emobe <anthony.r.shuker@gmail.com>
2018-11-11 13:49:48 +0000

    remove files

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as TodoItem } from './TodoItem/TodoItem';
-export { default as NewTodo } from './NewTodo/NewTodo';
 export { default as TodoList } from './TodoList/TodoList';
-export { default as Add } from './Add/Add';
+export { default as TodoModal } from './TodoModal/TodoModal';
